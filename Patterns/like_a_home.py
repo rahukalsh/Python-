56f42ca95047fbbddf44ef2c00491bac4cdbde60
Akash Chowdhury <156534244+rahukalsh@users.noreply.github.com>
2024-01-14 14:40:20 +0530

    It's look like a House

---
+++ Patterns/like_a_home.py
@@ -0,0 +1,20 @@
+# ----------  I think this Pattern is looks like a house !!!  --------
+
+num1 = 8
+for i in range(num1):
+    for j in range(i+1,num1):
+        print(" ", end=' ')
+    for j in range(i+1):
+        print("❄", end=' ')
+    for j in range(i):
+        print("❄", end=' ')
+    print('')
+
+num2 = 10
+for i in range(num2):
+    for j in range(num2):
+        if(i==0 or j==0 or i==num2-1 or j==num2-1):
+            print("🧱", end='')
+        else:
+            print("  ", end=' ')
+    print('')
