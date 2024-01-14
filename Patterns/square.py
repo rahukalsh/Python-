1d79a796fd973505b78b5888206aee6c14d589e1
Akash Chowdhury <156534244+rahukalsh@users.noreply.github.com>
2024-01-14 14:18:30 +0530

    Create square pattern
    It's a simple square pattern

---
+++ Patterns/square.py
@@ -0,0 +1,8 @@
+# -------- Simple Basic Patterns ---------
+
+num = int(input("Enter a number for Pattern : \n"))
+for i in range(num):
+    for j in range(num):
+        print("*",end=" ")
+    print("")
+    
